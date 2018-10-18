d4d6edf6dda421fde786243161c9c18eacf242c7
MacBook Pro5 <macbookpro5@MacBooks-MacBook-Pro-99.local>
2018-10-18 14:49:10 -0400

    Created front end calls
    AJAX calls Created
    UI .then .catch functions Created
    event handler created

---
+++ assets/scripts/api.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store.js')
+
+const signUp = function (userData) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: userData
+  })
+}
+
+const signIn = function (userData) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: userData
+  })
+}
+
+const changePassword = function (passwordData) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
+    method: 'PATCH',
+    data: passwordData
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
+// const newGame = function () {
+//   return $.ajax({
+//     url: config.apiUrl + '/games',
+//     method: 'POST',
+//     headers: {
+//       Authorization: `Token token=${store.user.token}`
+//     }
+//   })
+// }
+//
+// const getGames = function (data) {
+//   return $.ajax({
+//     url: config.apiUrl + `/games/`,
+//     headers: {
+//       Authorization: `Token token=${store.user.token}`
+//     },
+//     method: 'GET'
+//   })
+// }
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+  // newGame,
+  // getGames
+}
---
+++ assets/scripts/ui.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const signUpSuccess = function () {
+  $('#display-message').html('Sign up successful')
+  $('#display-message').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+  $('#change-password-form').trigger('reset')
+}
+
+const signUpFailure = function () {
+  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').css('color', 'red')
+  $('#sign-up-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+  $('#change-password-form').trigger('reset')
+}
+
+const signInSuccess = function (response) {
+  $('#display-message').html('Sign in successful')
+  $('#display-message').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+  $('#change-password-form').trigger('reset')
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#change-password-form').show()
+  $('#sign-out-button').removeClass('hidden')
+  $('#change-password-form').show()
+}
+
+const signInFailure = function () {
+  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').css('color', 'red')
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+  $('#change-password-form').trigger('reset')
+  $('#change-password-form').hide()
+}
+
+const changePasswordSuccess = function (response) {
+  $('#display-message').html('Change password succesful')
+  $('#display-message').css('color', 'green')
+  $('#change-password-form').trigger('reset')
+}
+
+const changePasswordFailure = function (response) {
+  $('#display-message').html('Password Failed to change, please try again')
+  $('#display-message').css('color', 'red')
+  $('#change-password-form').trigger('reset')
+}
+
+const signOutSuccess = function (response) {
+  $('#display-message').html('Signout Succesful')
+  $('#display-message').css('color', 'green')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+  $('#change-password-form').addClass('hidden')
+  $('#sign-out-button').addClass('hidden')
+  $('#new-game').addClass('hidden')
+  $('#message').addClass('hidden')
+  $('#change-password-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+  $('#change-password-form').hide()
+}
+
+const signOutFailure = function () {
+  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').css('color', 'red')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
