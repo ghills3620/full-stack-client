95c836b7ad4ef8e04c7506cc3a8095bb17b5aa64
MacBook Pro5 <macbookpro5@MacBooks-MacBook-Pro-99.local>
2018-10-23 15:28:53 -0400

    fixed bug

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -75,7 +75,6 @@ const onSearchWod = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.searchWod(data.wod)
-  console.log(data.wod)
     .then(ui.showWodSuccess)
     .catch(ui.getGameFailure)
 }
